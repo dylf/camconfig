8f6d6cb3702f4aedc611f6348cbc8d68cfdd4fe5
Dylan Fontaine <me@dylf.dev>
2023-07-01 11:15:53 -0700

    add tailwind and form elements for the device

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,73 @@
 use v4l::context;
 use v4l::prelude::*;
 
+#[derive(Debug, serde::Serialize)]
+struct VideoDevice {
+    name: String,
+    path: String,
+    index: usize,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct Controls {
+    id: u32,
+    name: String,
+    min: i64,
+    max: i64,
+    step: u64,
+    default: i64,
+    control_type: ControlType,
+    menu_items: Option<Vec<(u32, MItem)>>,
+}
+
+#[derive(Debug, serde::Serialize)]
+enum MItem {
+    Name(String),
+    Value(i64),
+}
+
+#[derive(Debug, serde::Serialize)]
+enum ControlType {
+    Integer,
+    Boolean,
+    Menu,
+    Button,
+    Integer64,
+    CtrlClass,
+    String,
+    Bitmask,
+    IntegerMenu,
+    U8,
+    U16,
+    U32,
+    Area,
+}
+
+fn control_type_to_enum(typ: v4l::control::Type) -> ControlType {
+    match typ {
+        v4l::control::Type::Integer => ControlType::Integer,
+        v4l::control::Type::Boolean => ControlType::Boolean,
+        v4l::control::Type::Menu => ControlType::Menu,
+        v4l::control::Type::Button => ControlType::Button,
+        v4l::control::Type::Integer64 => ControlType::Integer64,
+        v4l::control::Type::CtrlClass => ControlType::CtrlClass,
+        v4l::control::Type::String => ControlType::String,
+        v4l::control::Type::Bitmask => ControlType::Bitmask,
+        v4l::control::Type::IntegerMenu => ControlType::IntegerMenu,
+        v4l::control::Type::U8 => ControlType::U8,
+        v4l::control::Type::U16 => ControlType::U16,
+        v4l::control::Type::U32 => ControlType::U32,
+        v4l::control::Type::Area => ControlType::Area,
+    }
+}
+
+fn menu_item_to_enum(item: &v4l::control::MenuItem) -> MItem {
+    match item {
+        v4l::control::MenuItem::Name(name) => MItem::Name(name.to_string()),
+        v4l::control::MenuItem::Value(value) => MItem::Value(*value),
+    }
+}
+
 #[tauri::command]
 fn get_devices() -> Vec<VideoDevice> {
     let devices = context::enum_devices()
@@ -14,6 +81,7 @@ fn get_devices() -> Vec<VideoDevice> {
             VideoDevice {
                 name: name.to_string(),
                 path: path.to_string(),
+                index: dev.index(),
             }
         })
         .collect::<Vec<VideoDevice>>();
@@ -27,17 +95,40 @@ fn get_device_capabilities(path: String) -> String {
     caps.to_string()
 }
 
-#[derive(Debug, serde::Serialize)]
-struct VideoDevice {
-    name: String,
-    path: String,
+#[tauri::command]
+fn get_device_controls(path: String) -> Result<Vec<Controls>, String> {
+    let dev = Device::with_path(path).map_err(|e| format!("{}", e))?;
+    let controls = dev.query_controls().map_err(|e| format!("{}", e))?;
+
+    Ok(controls
+        .iter()
+        .map(|c| Controls {
+            id: c.id,
+            name: c.name.clone(),
+            min: c.minimum,
+            max: c.maximum,
+            step: c.step,
+            default: c.default,
+            control_type: control_type_to_enum(c.typ),
+            menu_items: match &c.items {
+                Some(items) => Some(
+                    items
+                        .iter()
+                        .map(|(i, item)| (*i, menu_item_to_enum(item)))
+                        .collect::<Vec<(u32, MItem)>>(),
+                ),
+                None => None,
+            },
+        })
+        .collect::<Vec<Controls>>())
 }
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             get_device_capabilities,
-            get_devices
+            get_devices,
+            get_device_controls
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { createSignal, onMount, For } from "solid-js";
+import { createSignal, onMount, For, Switch, Match } from "solid-js";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 
 type Device = {
   name: string;
   path: string;
+  index: number;
+};
+
+type Controls = {
+  id: number;
+  name: string;
+  min: number;
+  max: number;
+  step: number;
+  default: number;
+  control_type: "Integer" | "Boolean" | "Menu" | "CtrlClass";
 };
 
 function App() {
   const [devCapabilities, setDevCapabilities] = createSignal("");
   const [devices, setDevices] = createSignal<Device[]>([]);
+  const [controls, setControls] = createSignal<Controls[]>([]);
   const [selectedDevice, setSelectedDevice] = createSignal<string>();
 
   async function selectDevice() {
     setDevCapabilities(
       await invoke("get_device_capabilities", { path: selectedDevice() })
     );
+    setControls(
+      await invoke("get_device_controls", { path: selectedDevice() })
+    );
+    console.log(controls());
   }
 
   onMount(async () => {
     setDevices(await invoke("get_devices"));
+    setControls(await invoke("get_device_controls", { path: "/dev/video0" }));
     setSelectedDevice(devices()[0].path);
   });
 
   return (
-    <div class="container">
+    <div class="bg-zinc-800 text-white">
       <div class="row">
-        <h1>Cam config</h1>
+        <h1 class="text-3xl">Cam config</h1>
       </div>
-
       <select
+        class="form-select bg-zinc-700 text-white"
         name="device"
         id="device-select"
         onChange={(e) => setSelectedDevice(e.target.value)}
@@ -38,10 +54,57 @@ function App() {
           {(device) => (
             <option
               value={device.path}
-            >{`${device.name} (${device.path})`}</option>
+            >{`${device.index} ${device.name} (${device.path})`}</option>
           )}
         </For>
       </select>
+
+      <For each={controls()}>
+        {(control) => (
+          <div class="block mb-2">
+            <Switch fallback={<div>Unknown control</div>}>
+              <Match when={control.control_type === "CtrlClass"}>
+                <div class="text-xl">{control.name}</div>
+              </Match>
+              <Match when={control.control_type === "Menu"}>
+                <label>
+                  {control.name}
+                  <select class="form-select block bg-zinc-700">
+                    <For each={control.menu_items}>
+                      {(item) => (
+                        <option value={item[0]}>{item[1].Name}</option>
+                      )}
+                    </For>
+                  </select>
+                </label>
+              </Match>
+              <Match when={control.control_type === "Boolean"}>
+                <label>
+                  {control.name}
+                  <input
+                    class="form-checkbox rounded p-2 block"
+                    type="checkbox"
+                    name={control.name}
+                  />
+                </label>
+              </Match>
+              <Match when={control.control_type === "Integer"}>
+                <label>
+                  {control.name}
+                  <input
+                    type="range"
+                    class="form-range block"
+                    min={control.min}
+                    max={control.max}
+                    step={control.step}
+                    value={control.default}
+                  />
+                </label>
+              </Match>
+            </Switch>
+          </div>
+        )}
+      </For>
       <form
         class="row"
         onSubmit={(e) => {
@@ -49,9 +112,13 @@ function App() {
           selectDevice();
         }}
       >
-        <button type="submit">Get Device Capabilities</button>
+        <button
+          type="submit"
+          class="bg-zinc-700 rounded-md p-4 my-2 hover:bg-zinc-900"
+        >
+          Get Device Capabilities
+        </button>
       </form>
-
       <p>{devCapabilities()}</p>
     </div>
   );
